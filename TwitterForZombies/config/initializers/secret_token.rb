4034b2e02695fb97df17a84f5bbc95be1fb43920
saasbook <saasbook@saasbook.(none)>
2013-06-22 10:54:06 -0700

    Twitter for Zombies-A RoR application

---
+++ TwitterForZombies/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterForZombies::Application.config.secret_token = '0b3dfcf4a4750ee1881d4aceb2e0cd097be4b814bf08d48674a1559d4ec63054ea0d950063e7f5d35b53732d6514265bd4b9ada25729391e94e5b5fd4e17704f'
